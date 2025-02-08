88886043cc2be31808c879b1412f363f69d8ae72
sahi755 <sahilbhosale162@gmail.com>
2025-02-08 10:20:38 +0530

    get commit

---
+++ client/comp/Literal.jsx
@@ -0,0 +1,14 @@
+function Literals(){
+
+    const firstname="sahil";
+    const lastname="Bhosale";
+
+    const greeof=()=>{
+
+        return `my name is ${firstname} and my lastname is ${lastname}`;
+    }
+
+     return greeof;
+}
+
+export default Literals;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 
 import Arrow from '../comp/Arrow'
 import Greeet from '../comp/Greet'
+import Literal from '../comp/Literal'
 import './App.css'
 
 function App() {
 
   return (
+
     <>
       <div>
       
        <h1><Greeet></Greeet></h1>
        <h2><Arrow></Arrow></h2>
+       <h2><Literal></Literal></h2>
+      
       
       </div>
      
