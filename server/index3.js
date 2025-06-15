6669419e4949f242728a0774f97905ed24c5e606
Sahi755 <sahilbhosale162@gmail>
2025-06-15 19:35:37 +0530

    get commit

--- server/index3.js
+++ server/index3.js
@@ -1,1 +1,47 @@
-console.log("hello i love you")
+function variablechange(){
+
+    var name="Sahil"
+    console.log(`my name is ${name}`)
+    name="Manu bhakar"
+    console.log(`my name is ${name}`)
+}
+
+function block(){
+
+      let name="sahil"
+
+      {
+        name="siddhi"
+        console.log(`my name is ${name}`)
+      }
+
+      console.log(`my name is ${name}`)
+}
+
+function consts(){
+
+      const name="sahil"
+
+      console.log(`my name is ${name}`)
+
+      name="siddhi"// const is constant can't reassigned it 
+      console.log(`my name is ${name}`)
+}
+
+function obj(){
+
+      const employe={
+
+           name:"sahil",
+           id:"123",
+           post:"software Engineer",
+           company:"Google"
+      };
+
+      console.log(employe);
+      employe.company="facebook"
+       console.log(employe);
+}
+variablechange();
+block();
+obj();
