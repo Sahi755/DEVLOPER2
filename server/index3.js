faa73c4448913e69fd5a513b2385db0e4e645402
Sahi755 <sahilbhosale162@gmail>
2025-06-15 20:58:03 +0530

    get commit

--- server/index3.js
+++ server/index3.js
@@ -38,10 +38,76 @@ function obj(){
            company:"Google"
       };
 
-      console.log(employe);
-      employe.company="facebook"
+       console.log(employe);
+       employe.company="facebook"
        console.log(employe);
 }
+
+
+function typeofs(){
+
+      var password="Kunda@123"
+      console.log(`my password  type ${typeof(password)}`)
+}
+
+function typeof2(){
+
+      var phone=9420407141
+      console.log(`my password  type ${typeof(phone)}`)
+
+}
+
+function typeof3(){
+
+      var isMale=true;
+       console.log(`my password  type ${typeof(isMale)}`)
+}
+function typeof4(){
+
+       var bestfriendName=undefined
+       console.log(`my password  type ${typeof(bestfriendName)}`)
+}
+
+function typeof5(){
+     
+      var bestfriendName=null
+      console.log(`my password  type ${typeof(bestfriendName)}`)
+
+}
+
+function typeof6(){
+
+       var  BigInt = 687859499957485n;
+       console.log(`my password  type ${typeof(BigInt)}`)
+}
+
+function typeof7(){
+
+       var pi=3.14;
+       console.log(`my password  type ${typeof(pi)}`)
+
+}
+
+function typeof8(){
+
+       let pi=["kiran","siddhi","shruti","riddhi"]
+       console.log(`my password  type ${typeof(pi)}`)
+
+       let numbers = new Array(1, 2, 3, 4, 5);
+        console.log(`my password  type ${typeof(numbers)}`)
+}
+
 variablechange();
 block();
 obj();
+typeofs();
+typeof2();
+typeof3();
+typeof4();
+typeof5();
+typeof6();
+typeof7();
+typeof8();
+
+
+
